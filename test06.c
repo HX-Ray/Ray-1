de40e6f496b44a0dc3397de03a2192911cb66115
HX-Ray <1923642727@qq.com>
2020-10-24 13:22:42 +0800

    1024

---
+++ test06.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void ppp(int a[],int n){
+	int i,j;
+	for (i=0;i<n;i++)
+	{
+		for (j=n-1;j>i;j--)
+		{
+			if(a[j]*a[j]<a[j-1]*a[j-1]){
+				int temp=a[j];
+				a[j]=a[j-1];
+				a[j-1]=temp;
+			}
+		}
+	}
+}
+
+
+int main(void){
+	
+	int n,i;
+	scanf("%d",&n);
+	int a[n];
+
+	for (i=0;i<n;i++)
+		scanf("%d",&a[i]);
+
+	ppp(a,n);
+	for (i=0;i<n;i++)
+		printf("%d ",a[i]);
+
+	printf("\n");
+    	return 0;
+}
